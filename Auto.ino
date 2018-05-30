bb63e26e6c681c1669a43f6882737e495e925f11
Nozols <nozols0@gmail.com>
2018-05-30 17:21:58 +0200

    Move motor functions, rename Sensors to Modules

--- Auto.ino
+++ Auto.ino
@@ -3,19 +3,14 @@
 
 #include <Wire.h>
 #include <Servo.h>
-#include <QTRSensors.h>
 #include "Util/Logger.h"
 #include "MPU6050/src/MPU6050_tockn.h"
-#include "Sensors/UltrasoneSensor.h"
-#include "Sensors/LineSensor.h"
-#include "Sensors/TemperatureSensor.h"
+#include "Modules/UltrasoneSensor.h"
+#include "Modules/LineSensor.h"
+#include "Modules/TemperatureSensor.h"
+#include "Modules/MotorControl.h"
 
 MPU6050 gyro(Wire);
-Servo steerServo;
-Servo motorServo;
-
-#define SERVOPIN 11
-#define MOTORPIN 23
 
 bool canStart = true;
 
@@ -37,8 +32,7 @@ void setup()
   Logger::info("Finished initializing ultrasone sensor");
 
   Logger::info("Initializing steering servo");
-  steerServo.attach(SERVOPIN);
-  motorServo.attach(MOTORPIN);
+  motor_init();
   temp_init();
   Logger::info("Finished initializing steering servo");
 
@@ -63,32 +57,4 @@ void loop()
   Logger::data("gx", gyro.getAngleX());
   Logger::data("temp", temp_isTemperatureTooHigh());
   Logger::data("steer", qtr_getSteerAmount());
-}
-
-/**
- * Control the steering servo
- * TODO check min and max steering angle
- * @param int turnPercentage 0-50 left, 50-100 right
- */
-void controlServo(int turnPercentage)
-{
-  int turnServoValue = map(turnPercentage, 0, 100, 0, 180);//convert procents to Servo value
-  steerServo.write(turnServoValue);
-}
-
-/**
- * Control the main motor
- * @param int speedPercentage 0 is stop, 100 full throttle
- */
-void controlMotor(int speedPercentage)
-{
-  if(temp_isTemperatureTooHigh())
-  {//check if the temperature is too high
-    speedPercentage = map(speedPercentage, 0, 100, 0, 50);//reduce the speed when to hot
-  }
-
-  int speedPWMValue = map(speedPercentage, 0, 100, 0, 255);//convert procents to PWM value
-  //analogWrite(MOTORPIN, speedPWMValue);
-  Logger::info(speedPWMValue);
-  motorServo.write(speedPWMValue);
 }
---
+++ Modules/MotorControl.h
@@ -0,0 +1,50 @@
+/*
+ * @author Peter van Breugel
+ * @date 15-05-18
+ * @description functions for the motor module
+ */
+#include "Arduino.h"
+#include <Servo.h>
+
+#define MOTOR_SERVO_PIN 11
+#define MOTOR_MOTOR_PIN 23
+
+Servo motor_steerServo;
+Servo motor_motorServo;
+
+/**
+ * Initialize motor modules
+ */
+void motor_init()
+{
+  motor_steerServo.attach(MOTOR_SERVO_PIN);
+  motor_steerServo.attach(MOTOR_MOTOR_PIN);
+}
+
+/**
+ * Control the steering servo
+ * TODO check min and max steering angle
+ * @param int turnPercentage 0-50 left, 50-100 right
+ */
+void motor_controlServo(int turnPercentage)
+{
+  int turnServoValue = map(turnPercentage, 0, 100, 0, 180);//convert procents to Servo value
+  motor_steerServo.write(turnServoValue);
+}
+
+/**
+ * Control the main motor
+ * @param int speedPercentage 0 is stop, 100 full throttle
+ */
+void motor_controlMotor(int speedPercentage)
+{
+  if(temp_isTemperatureTooHigh())
+  {//check if the temperature is too high
+    speedPercentage = map(speedPercentage, 0, 100, 0, 50);//reduce the speed when to hot
+  }
+
+  int speedPWMValue = map(speedPercentage, 0, 100, 0, 255);//convert procents to PWM value
+  //analogWrite(MOTOR_MOTOR_PIN, speedPWMValue);
+  Logger::info(speedPWMValue);
+  motor_motorServo.write(speedPWMValue);
+}
